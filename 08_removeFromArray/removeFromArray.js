26db6998f2acf6edacaa92d69d11abe35af53a11
divyahr <88703476+divyahr@users.noreply.github.com>
2025-05-15 00:11:09 +0530

    complete removeFromArray exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...vals) {
+    for (i=0; i<arr.length; i++){
+        for (let val of vals){
+            if(arr[i] === val){
+                arr.splice(i,1);
+                i--;
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
