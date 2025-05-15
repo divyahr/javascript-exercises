b67daf5c05f30e52a73e9387555a7a03a00fdb9d
divyahr <88703476+divyahr@users.noreply.github.com>
2025-05-15 14:05:54 +0530

    complete tempConversion exercise

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(numInF) {
+  return Number((((numInF - 32)*5)/9).toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(numInC) {
+  return Number(((numInC * 9/5) + 32).toFixed(1))
 };
 
 // Do not edit below this line
