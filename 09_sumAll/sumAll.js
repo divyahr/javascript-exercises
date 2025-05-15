9f1144c5cabe8d2d8af0a0a8ace22014ae3893a3
divyahr <88703476+divyahr@users.noreply.github.com>
2025-05-15 10:08:31 +0530

    complete sumAll exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if(num1<num2 && Number.isInteger(num1) && Number.isInteger(num2) && num1>0 && num2>0 && !Number.isNaN(num1) && !Number.isNaN(num2) ){
+        for(i=num1; i<=num2; i++){
+            sum += i
+        }
+    }else if(num1>num2){
+        for(i=num2; i<=num1; i++){
+            sum += i
+        }
+    }else{
+        return 'ERROR'
+    }
+    return sum
 };
 
 // Do not edit below this line
